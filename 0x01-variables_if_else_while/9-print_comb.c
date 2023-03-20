210ab05789e4e94ac0870758fdd614da163b4899
BernardNjuguna <bengitau1@gmail.com>
2023-03-20 15:05:30 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - digits combinations
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0 ; b < 10 ; b++)
+	{
+		if (b == 9)
+			putchar(b + '0');
+		else
+		putchar(b + '0');
+		putchar(',');
+		putchar(' ');
+		return (0);
+	}
+}
+
